a62fe9a2840c386e7750eda5c20a249c16d7630a
Diego12132025 <diegowork2004@gmail.com>
2025-04-01 12:37:17 +0100

    añadiendo CheckIn y CheckOut

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -8,7 +8,9 @@ public class Reserva {
     private int idReserva;
     private int numeroHabitacion;
     private Cliente cliente;            // Cliente que realiza la reserva
-    private LocalDateTime fechaInicio;
-    private LocalDateTime fechaFin;     // Máximo de 3 meses de reserva
+    private LocalDateTime fechaInicioReserva;   
+    private LocalDateTime fechaFinReserva; // Máximo de 3 meses de reserva
+    private LocalDateTime fechaCkeckIn;
+    private LocalDateTime fechaCheckOut;     
     private double precioTotal;         // Precio total de la reserva (depende del tipo de habitación y número de noches)
 }
